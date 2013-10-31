7ddec378ea134520ceb4e4ebd044394d49240ef6
Tanay Mehta <mehtatanay@gmail.com>
2013-10-31 15:56:56 -0400

    added initial project

---
+++ StockMarket1.0/src/stockmarket/g0/G3Player.java
@@ -0,0 +1,28 @@
+package stockmarket.g0;
+
+import java.util.ArrayList;
+
+import stockmarket.sim.EconomicIndicator;
+import stockmarket.sim.Player;
+import stockmarket.sim.Portfolio;
+import stockmarket.sim.Stock;
+import stockmarket.sim.Trade;
+
+public class G3Player extends Player {
+
+	@Override
+	public void learn(ArrayList<EconomicIndicator> indicators,
+			ArrayList<Stock> stocks) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayList<Trade> placeTrade(int currentRound,
+			ArrayList<EconomicIndicator> indicators, ArrayList<Stock> stocks,
+			Portfolio porfolioCopy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
